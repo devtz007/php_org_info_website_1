844546662f64f357f8ee4463b406d1b737415cbb
devtz007 <bluestacktazin@gmail.com>
2022-12-11 23:58:19 +0600

    word fix

--- ornarigram_page.php
+++ ornarigram_page.php
@@ -1,7 +1,7 @@
 <?php require './header.php' ?>
 <main>
 <section class="organigram">
-    <div class="section-heading">Present Organigram</div>
+    <div class="section-heading">Present Organogram</div>
     <div class="organigram-image-cont">
         <img src="./assets/components/organigram_comp/images/orga.png" alt="">
     </div>
